0188a33063f84e5526c36645d5110a1e623b6ea0
Victor Gabriel Pantaleão Santos <53866405+imgabrielvi@users.noreply.github.com>
2021-11-24 15:44:48 -0300

    Create lab02.py

---
+++ Laboratório 02/lab02.py
@@ -0,0 +1,6 @@
+#####################################################
+# MC102 - Algoritmos e Programação de Computadores
+# Laboratório 1 - Formula 1
+# Nome: Victor Gabriel Pantaleão Santos
+# RA: 248198
+#####################################################
