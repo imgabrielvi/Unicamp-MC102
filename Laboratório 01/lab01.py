a1b7ea96be470da17224e6432275e88a064aad32
Victor Gabriel Pantaleão Santos <53866405+imgabrielvi@users.noreply.github.com>
2021-11-24 14:25:35 -0300

    Create lab01.py

---
+++ Laboratório 01/lab01.py
@@ -0,0 +1,17 @@
+#####################################################
+# MC102 - Algoritmos e Programação de Computadores
+# Laboratório 1 - Aritmética com Inteiros
+# Nome: Victor Gabriel Pantaleão Santos
+# RA: 248198
+#####################################################
+
+a = int (input())               # Recebe valor de 'a'
+b = int (input())               # Recebe valor de 'b'
+print('a', a, sep = ' = ')
+print('b', b, sep = ' = ')
+print("a + b", a + b, sep = ' = ')
+print("a - b", a - b, sep = ' = ')
+print("a * b", a * b, sep = ' = ')
+print("a ** b", a ** b, sep = ' = ')
+print("a // b", a // b, sep = ' = ')
+print("a % b", a % b, sep = ' = ')
